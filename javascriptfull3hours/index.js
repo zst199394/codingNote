3a729f9215ab03f649135f6a67f622f977ee835e
shuting <zst9394@outlook.com>
2022-10-21 13:53:53 -0400

    practice var

--- javascriptfull3hours/index.js
+++ javascriptfull3hours/index.js
@@ -5,3 +5,17 @@ console.log(number);
 /* 
 this is a multi-line comment
 */
+
+var myName = "Moo";
+myName  = 4;
+console.log(myName);
+
+let ourName = "weee";
+ourName = 7;
+console.log(ourName);
+
+const pi = 3.14 ;
+/* const variable  cannot be changed 
+*/
+
+
