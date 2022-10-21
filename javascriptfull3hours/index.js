f985929190c370ea9c751e8688542e4d576390b5
shuting <zst9394@outlook.com>
2022-10-21 14:00:34 -0400

    adding var  abc

--- javascriptfull3hours/index.js
+++ javascriptfull3hours/index.js
@@ -14,8 +14,16 @@ let ourName = "weee";
 ourName = 7;
 console.log(ourName);
 
-const pi = 3.14 ;
-/* const variable  cannot be changed 
-*/
+const pi = 3.14 ; // const variable  cannot be changed 
 
+var a;
+var b = 5;
+console.log(a);  // null? undefined
+a = 1;
+b = a;
+c = "I am a "
+console.log(a);
 
+console.log(a+1);
+console.log(b*8);
+console.log(c+"string");
