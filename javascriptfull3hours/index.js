6880b024f56e166fc00b794e31439a078e480a3b
shuting <zst9394@outlook.com>
2022-10-26 22:35:46 -0400

    add javascript note += -= /=...

--- javascriptfull3hours/index.js
+++ javascriptfull3hours/index.js
@@ -20,10 +20,55 @@ var a;
 var b = 5;
 console.log(a);  // null? undefined
 a = 1;
-b = a;
-c = "I am a "
+b = a++;
+c = "I am a ";
 console.log(a);
 
 console.log(a+1);
 console.log(b*8);
 console.log(c+"string");
+
+var studyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+var sum = 10 + 0; //adding numbers
+console.log("sum = " + sum);
+
+var difference = 4 - 1; //sustracting numbers
+console.log("difference = " + difference);
+
+var product = 6 * 3//multiplying numbers
+console.log("product = " + product);
+
+var quotient = 66 / 33; //dividing numbers
+console.log("quotient = " + quotient);  
+
+var myVar = 8; //incrementing numbers
+myVar++;
+console.log(myVar);
+
+var myVar = 11;//decrementing numbers
+myVar-- ;
+console.log(myVar);
+
+var ourDecimal = 5.8;//decimal numbers
+var myDecimal = 0.009;
+console.log(ourDecimal,myDecimal);
+
+var product = 2.5 * 2.0;//multiply decimals
+console.log(product);
+
+var quotient = 8.8 / 2.2;//divide decimals
+console.log(quotient);
+
+var remainder = 11;//finding a remainder
+remainder = 50 % 3;
+console.log(remainder); // when remainder =0 means its even -- or odd
+
+var f = 7; // += or -= or *= or /=
+f+=3;
+console.log(f);
+
+
+
