8df917362f0bc7bc9fd7ace1394fb19eae24f2c4
shuting <zst9394@outlook.com>
2022-11-10 14:10:22 -0500

    default in switch

--- javascriptfull3hours/index.js
+++ javascriptfull3hours/index.js
@@ -562,3 +562,26 @@ function caseinSwitch(val){
      return answer;
     }
 console.log("caseinSwitch: "+ caseinSwitch(2));
+
+
+/* Default Option in Switch Statements  */
+
+function switchOfStuff(val){
+    var answer = "";
+    switch(val){
+        case "a":
+          answer = "apple";
+          break;
+        case "b":
+            answer = "bird";
+            break;
+        case "c":
+            answer = "cat";
+            break;
+        default:  // if we pass sth else   without this line: answer =""
+            answer = "stuff!";
+            break;
+    }
+    return answer;
+}
+console.log("switchOfStuff: " + switchOfStuff(""));
