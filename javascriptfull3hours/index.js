b075d6eae9835f901fd167cf45cc1a4242414119
shuting <zst9394@outlook.com>
2022-11-10 13:43:27 -0500

    gold code ! !!!!!!

--- javascriptfull3hours/index.js
+++ javascriptfull3hours/index.js
@@ -520,17 +520,17 @@ function golfScore(par, strokes){
         return names[0]
     } else if(strokes <= par -2){
         return names[1]
-    } else if(strokes <= par -1){
+    } else if(strokes == par -1){
         return names[2]
-    } else if(strokes <= par){
+    } else if(strokes == par){
         return names[3]
-    } else if(strokes <= par +1){
+    } else if(strokes == par +1){
         return names[4]
-    } else if(strokes <= par +2){
+    } else if(strokes == par +2){
         return names[5]
-    } else if(strokes <= par +3){
+    } else if(strokes == par +3){
         return names[6]
     }
 
 }
-console.log("golfScores : " + golfScore(5,4))
+console.log("golfScores : " + golfScore(5,2));
