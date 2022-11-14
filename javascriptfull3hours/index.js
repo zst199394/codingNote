618073bebe782117099c7b6298800e071182c758
shuting <zst9394@outlook.com>
2022-11-14 10:48:14 -0500

    if else chains to switch

--- javascriptfull3hours/index.js
+++ javascriptfull3hours/index.js
@@ -620,3 +620,51 @@ console.log("sequentialSizes:  " + sequentialSizes(4));
 
 
 /* Replacing If Else Chains with Switch  */
+function chainToSwitch(val){
+    var answer = "";
+
+    if (val === "bob") {  // 
+        answer = "Marley";
+    } else if (val === 42 ) {
+        answer = "The Answer";
+    } else if (val === 1 ) {
+        answer = "There is no #1";
+    } else if (val === 99) {
+        answer = "Missed me by this much!";
+    } else if (val === 7) {
+        answer = "Ate Nine";
+    }
+
+    return answer;
+}
+console.log("chainToSwitch: " + chainToSwitch(1));
+
+function chainToSwitch(val){
+    var answer = "";
+
+    switch(val){
+      case "bob":
+        answer = "Marley";
+        break;
+      case 42:
+        answer = "The Answer";
+        break;
+      case 1:
+        answer = "There is no #1";
+        break;
+      case 99:
+        answer = "Missed me by this much!";
+        break;
+      case 7:
+        answer = "Ate Nine";
+        break;
+      case 8:
+        answer = "888888...";
+        break;
+        default :
+        answer = "Stuff ?";
+    }
+
+    return answer;
+}
+console.log("chainToSwitch: " + chainToSwitch(8));
