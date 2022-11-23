67111fb6f4e7dbc60e1e44d76f6aae70caa68903
shuting <zst9394@outlook.com>
2022-11-23 00:21:44 -0500

    Accessing Nested Arrays

--- javascriptfull3hours/index.js
+++ javascriptfull3hours/index.js
@@ -982,5 +982,25 @@ var gloveBoxContents = myStorage.car.inside["glove box"];
 console.log(gloveBoxContents);
 
 /** Accessing Nested Arrays */
+var myPlants = [
+    {
+      type: "flowers",
+      list: [
+        "rose",
+        "tulip",
+        "dandelion"
+        ]
+    },
+    {
+      type: "trees",
+      list:[
+        "fir",
+        "pine",
+        "birch"
+      ]
+    }
+];
 
+var secondTree = myPlants[1].list[1];
+console.log(secondTree);
 
