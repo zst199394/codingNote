8a613a81c18f962ded01a757d2305562aa0c89f9
shuting <zst9394@outlook.com>
2023-01-13 00:33:02 -0500

    Use Arrow Functions to Write Concise Anonymous Functions

--- javascriptfull3hours/index.js
+++ javascriptfull3hours/index.js
@@ -1510,3 +1510,12 @@ const PI = freezeObj();
 console.log(PI);
 
 /** Use Arrow Functions to Write Concise Anonymous Functions  */
+
+//  var magic = function() {
+//     return new Date();
+// };  
+
+const magic = () => new Date();
+ console.log(magic);
+
+ 
