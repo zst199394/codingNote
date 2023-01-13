b558dfb47e5834c2d94128163603b9a0ee83a523
shuting <zst9394@outlook.com>
2023-01-13 00:42:59 -0500

    Write Arrow Functions with Parameters

--- javascriptfull3hours/index.js
+++ javascriptfull3hours/index.js
@@ -1519,3 +1519,14 @@ const magic = () => new Date();
  console.log(magic);
 
  
+
+/** Write Arrow Functions with Parameters */
+
+//  var myConcat = function(arr1, arr2) {
+//     return arr1.concat(arr2);
+// };
+
+// console.log(myConcat([1,2],[3,4,5]));
+
+const myConcat = (arr1,arr2) => arr1.concat(arr2);
+console.log(myConcat([1,2],[3,4,5])); 
