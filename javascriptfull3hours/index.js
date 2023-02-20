b40de14cf0e7ba04290239eae724a3b305833aa0
shuting <zst9394@outlook.com>
2023-02-20 00:58:14 -0500

    n

--- javascriptfull3hours/index.js
+++ javascriptfull3hours/index.js
@@ -1517,7 +1517,7 @@ console.log(PI);
 
 const magic = () => new Date();
  console.log(magic);
-
+ 
 /** Write Arrow Functions with Parameters */
 
 //  var myConcat = function(arr1, arr2) {
@@ -1527,3 +1527,4 @@ const magic = () => new Date();
 
 const myConcat = (arr1,arr2) => arr1.concat(arr2);
 console.log(myConcat([1,2],[3,4,5])); 
+
